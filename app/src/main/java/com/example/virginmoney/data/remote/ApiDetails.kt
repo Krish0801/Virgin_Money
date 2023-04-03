f2887341e84848c7de63852ce3f09fa90a3509cc
praveenkrishnamoorthy <praveenkrishang@gmail.com>
2023-04-03 09:23:40 +0100

    UI

--- app/src/main/java/com/example/virginmoney/data/remote/ApiDetails.kt
+++ app/src/main/java/com/example/virginmoney/data/remote/ApiDetails.kt
@@ -2,10 +2,7 @@ package com.example.virginmoney.data.remote
 
 object ApiDetails {
 
-    /*const val BASE_URL = "https://61e947967bc0550017bc61bf.mockapi.io/api/v1/"
+    const val BASE_URL = "https://61e947967bc0550017bc61bf.mockapi.io/api/v1/"
     const val PEOPLE = "people/"
-    const val ROOMS = "rooms/"*/
-    const val BASE_URL = "https://642958715a40b82da4d0f7d5.mockapi.io/api/v1/"
-    const val PEOPLE = "users/"
-    const val ROOMS = "users/"
+    const val ROOMS = "rooms/"
 }
